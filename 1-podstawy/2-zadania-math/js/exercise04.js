e5e80bce14b45d11f1795adf29783a264063a608
Marcin Domański <kartofelek007@gmail.com>
2019-12-15 17:12:50 +0100

    poprawka w zadaniu 4 + komunikat w popupie

--- 1-podstawy/2-zadania-math/js/exercise04.js
+++ 1-podstawy/2-zadania-math/js/exercise04.js
@@ -6,8 +6,20 @@
     console.log(Math.tan(1));
     console.log(Math.PI);
 
-    console.log(Math.random(Math.sin(1)));
-    console.log(Math.random(Math.cos(1)));
-    console.log(Math.random(Math.tan(1)));
-    console.log(Math.random(Math.PI));
+    console.log(Math.round(Math.sin(1)));
+    console.log(Math.floor(Math.sin(1)));
+    console.log(Math.ceil(Math.sin(1)));
+
+    console.log(Math.round(Math.cos(1)));
+    console.log(Math.floor(Math.cos(1)));
+    console.log(Math.ceil(Math.cos(1)));
+
+    console.log(Math.round(Math.tan(1)));
+    console.log(Math.floor(Math.tan(1)));
+    console.log(Math.ceil(Math.tan(1)));
+
+    console.log(Math.round(Math.PI));
+    console.log(Math.floor(Math.PI));
+    console.log(Math.ceil(Math.PI));
+
 }
